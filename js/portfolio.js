2d0010538b086711b45dd8d062ca377d07e0117a
Gwenhwhy <angelkr96@rambler.ru>
2016-10-18 01:12:56 +0300

    резюме попап + смена скроллов + иконки

--- js/portfolio.js
+++ js/portfolio.js
@@ -10,4 +10,40 @@ function animate (elem) {
               animate ($(this));
             });
 
-          });
+          }); //animation
+
+$(document).ready(
+
+  function() { 
+
+    $("body").niceScroll({cursorcolor: '#33c1e3', mousescrollstep: '60'});
+    $(".user-aboutme-text").niceScroll({cursorcolor: '#33c1e3', 
+    cursoropacitymin: '1', cursoropacitymax: '0', cursorwidth: '10px'});
+
+
+  }
+
+);
+ //scroll
+
+
+
+ $(document).ready(function() {
+    $('#overlay').css({opacity: 0.5}); // Делаем затемняющий фон кроссбраузерным
+    positionCenter($('#popup')); // Позиционируем всплывающее окно по центру
+	
+    $('#show_popup').click(function() { // При клике по ссылке, показываем всплывающее окно
+        $('#popup, #overlay').fadeIn(300);
+    });
+	
+    $('#close_popup').click(function() { // Скрываем всплывающее окно при клике по кнопке закрыть
+		$('#popup, #overlay').fadeOut(300);
+    });
+	
+    function positionCenter(elem) { // Функция, которая позиционирует всплывающее окно по центру
+        elem.css({
+            marginTop: '-' + elem.height() / 2 + 'px',
+			marginLeft: '-' + elem.width() / 2 + 'px'
+        });
+    }
+}); //popup
